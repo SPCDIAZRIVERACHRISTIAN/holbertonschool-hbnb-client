11198ca93e3f643eeab734d82396468b31abace7
Christian <christianda3@gmail.com>
2024-07-31 14:38:42 -0400

    chore: Update HTML files with new review section and button

--- base_files/mock-api/app.py
+++ base_files/mock-api/app.py
@@ -11,10 +11,10 @@ app.config.from_object('config.Config')
 jwt = JWTManager(app)
 CORS(app) # Enable CORS for all routes
 
-with open('mock-api/data/users.json') as f:
+with open('base_files/mock-api/data/users.json') as f:
     users = json.load(f)
 
-with open('mock-api/data/places.json') as f:
+with open('base_files/mock-api/data/places.json') as f:
     places = json.load(f)
 
 # In-memory storage for new reviews
