58fb5c99dc43fefe4f77d0e267c499291e074beb
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-07-31 12:15:28 -0400

    chore: Update HTML files with new review section and button

--- base_files/script/script.js
+++ base_files/script/script.js
@@ -4,5 +4,26 @@
 */
 
 document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
+    const loginForm = document.getElementById('login-form');
+    if (loginForm) {
+      loginForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        async function loginUser(email, password) {
+          const response = await fetch('http://127.0.0.1:5000/login', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ email, password })
+          });
+          if (response.ok) {
+            const data = await response.json();
+            document.cookie = `token=${data.access_token}; path=/`;
+            window.location.href = 'index.html';
+        } else {
+            alert('Login failed: ' + response.statusText);
+        }
+      }
+      });
+    }
   });
